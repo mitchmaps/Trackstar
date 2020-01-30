aab369639d7a4e905025a39d15891a22c65b9b27
Mitchell Soares <mitchell.soares@shopify.com>
2020-01-30 10:53:19 -0500

    Added dependencies for react-navigation and created containers

--- Trackstar/App.tsx
+++ Trackstar/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+import { CourseDashboard } from "./components/CourseDashboard/CourseDashboard";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const AppNavigator = createBottomTabNavigator({
+  CourseDashboard: { screen: CourseDashboard }
 });
+
+const AppContainer = createAppContainer(AppNavigator);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
---
+++ Trackstar/components/CourseDashboard/CourseDashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {Text} from 'react-native';
+
+export class CourseDashboard extends React.Component {
+  render() {
+    return <Text>Hello this is a tab</Text>;
+  }
+}
