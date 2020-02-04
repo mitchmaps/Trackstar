f4cb8039afeb9a92324d8ea6db6abc1ae26aa3c9
Nareen <Nareen.Khurshid@carleton.ca>
2020-02-04 13:50:33 -0500

    Testing commit
    Just put some comments so later on I'll remember what I was doing

--- Trackstar/components/Courses/components/CourseView.tsx
+++ Trackstar/components/Courses/components/CourseView.tsx
@@ -9,6 +9,15 @@ export class CourseView extends React.Component {
   }
 
   gradeCalculations() {
-    // Where the logic for calculating grades goes
+     // Where the logic for calculating grades goes
+    
+    /* planning first
+    total grade for the course starts at 0
+    user enters grades for each evaluation. How does this relation work? does it need to know about evaluations
+    yeah it needs to get the percentage information from it
+    adds up to total grade
+    */
+   
+   
   }
 }
