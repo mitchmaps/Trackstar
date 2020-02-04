35569e26118276f077f2ae4b2532579bd3e3277d
Nareen <Nareen.Khurshid@carleton.ca>
2020-02-04 13:53:59 -0500

    Update CourseView.tsx

--- Trackstar/components/Courses/components/CourseView.tsx
+++ Trackstar/components/Courses/components/CourseView.tsx
@@ -10,13 +10,15 @@ export class CourseView extends React.Component {
 
   gradeCalculations() {
      // Where the logic for calculating grades goes
+    float total_grade = 0; 
+    //for now = 0 maybe change it to = calculation of each evaluation*percentage worth
     
-    /* planning first
-    total grade for the course starts at 0
+    /* 
     user enters grades for each evaluation. How does this relation work? does it need to know about evaluations
     yeah it needs to get the percentage information from it
     adds up to total grade
     */
+    
    
    
   }
