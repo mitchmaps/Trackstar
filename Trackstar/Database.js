19b3955abcdc6f5bef0a86c7813c7d3e4c334724
Gaby Pancu <gaby.pancu@shopify.com>
2020-02-04 13:40:17 -0500

    set up

---
+++ Trackstar/Database.js
@@ -0,0 +1,53 @@
+import SQLite from "react-native-sqlite-storage";
+SQLite.DEBUG(true);
+SQLite.enablePromise(true);
+
+const database_name = "Reactoffline.db";
+const database_version = "1.0";
+const database_displayname = "SQLite React Offline Database";
+const database_size = 200000;
+
+export default class Database {
+
+  initDB() {
+    let db;
+    return new Promise((resolve) => {
+      console.log("Plugin integrity check ...");
+      SQLite.echoTest()
+        .then(() => {
+          console.log("Integrity check passed ...");
+          console.log("Opening database ...");
+          SQLite.openDatabase(
+            database_name,
+            database_version,
+            database_displayname,
+            database_size
+          )
+            .then(DB => {
+              db = DB;
+              console.log("Database OPEN");
+              db.executeSql('SELECT 1 FROM Product LIMIT 1').then(() => {
+                  console.log("Database is ready ... executing query ...");
+              }).catch((error) =>{
+                  console.log("Received error: ", error);
+                  console.log("Database not yet ready ... populating data");
+                  db.transaction((tx) => {
+                      tx.executeSql('CREATE TABLE IF NOT EXISTS Course (code, title, min_grade, cur_grade, time_slot, complete)');
+                  }).then(() => {
+                      console.log("Table created successfully");
+                  }).catch(error => {
+                      console.log(error);
+                  });
+              });
+              resolve(db);
+            })
+            .catch(error => {
+              console.log(error);
+            });
+        })
+        .catch(error => {
+          console.log("echoTest failed - plugin not functional");
+        });
+      });
+  };
+}
