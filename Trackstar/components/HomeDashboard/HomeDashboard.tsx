74e167ae6be5445d4c950727b72a58a55bf63ecb
haohaodu <hdu2899@gmail.com>
2020-02-18 15:48:39 -0500

    haohao's first demo for dashboard

--- Trackstar/components/HomeDashboard/HomeDashboard.tsx
+++ Trackstar/components/HomeDashboard/HomeDashboard.tsx
@@ -1,14 +1,92 @@
 import React from 'react';
-
-import { Text, View } from 'react-native';
 import Styles from './Styles';
 
+import { Alert, Text, View } from 'react-native';
+
+import { Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
+import { red100 } from 'react-native-paper/lib/typescript/src/styles/colors';
+import { iOSUIKit } from 'react-native-typography';
+
 export class HomeDashboard extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  
   render() {
+
+    const MyComponent = (props) => (
+
+      <Card onPress  ={ () =>
+        {
+          return Alert.alert('Task Title', '\nEstimated Duration' +
+            '\nDue Date' +
+            '\nCourse Code' +
+            '\nWeighting' +
+            '\nPriority Score', [
+            { text: 'Cancel', onPress: () => console.log('Cancel Pressed!') },
+            { text: 'OK', onPress: () => console.log('OK Pressed!') },
+          ], { cancelable: false });
+        }
+
+
+      } style = {[{backgroundColor: props.color}, Styles.container]}>
+        <Card.Content>
+          <View>
+            <Title>{props.title}</Title>
+            <Paragraph style={Styles.paragraphText}>{props.Duration}</Paragraph>
+            <Paragraph style={Styles.paragraphText}>
+              {props.DueDate}
+            </Paragraph>
+          </View>
+        </Card.Content>
+        <Card.Actions>
+          <Button onPress={() => Alert.alert(
+          
+          'Edit Page',
+          'Options for Editing',
+          [
+            {text: 'Cancel', onPress: () => console.log('Cancel Pressed!')},
+            {text: 'OK', onPress: () => console.log('OK Pressed!')},
+          ],
+          { cancelable: false }
+        )}>Edit</Button>
+        </Card.Actions>
+      </Card>
+    );
     return (
-      <View style={Styles.content}>
-        <Text>Home dashboard!</Text>
+      <View style = {Styles.content }>
+        
+        <Text style={Styles.dashboardText}>
+            Dashboard
+        </Text>
+
+        
+
+        
+        
+        <MyComponent 
+          title= "Task Title"
+          Duration="Estimated Duration"
+          DueDate="Due Date" 
+          color = '#FFFFFF'
+          />
+
+        
+        <MyComponent 
+          title= "Read 3004 Chapter 6"
+          Duration="Duration : 2 hours"
+          DueDate= "Due Date: 2020/03/01" 
+          color = '#FF99CC'
+        />
+        <MyComponent 
+          title= "Finish 2804 A2"
+          Duration="Duration : 2 hours"
+          DueDate="Due Date: 2020/03/11" 
+          color = '#FF9933'
+        />
       </View>
+      
     );
   }
 }
--- Trackstar/components/HomeDashboard/Styles.tsx
+++ Trackstar/components/HomeDashboard/Styles.tsx
@@ -1,9 +1,37 @@
 import { StyleSheet } from 'react-native';
+import { iOSUIKit } from 'react-native-typography';
+import { startClock } from 'react-native-reanimated';
 
 export default StyleSheet.create({
   content: {
     flex: 1, 
-    justifyContent: 'center', 
-    alignItems: 'center'
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    marginLeft: 20
+  },
+  container: {
+    shadowColor: 'rgba(0,0,0,1)',
+    shadowOffset: { height: 0, width: 0 },
+    shadowOpacity: 1, //default is 1
+    shadowRadius: 1, //default is 1
+    width: 220,
+    marginTop: 20
+  },
+  headingText: {
+    fontSize: 30
+  },
+  titleText: {
+    fontSize: 20,
+    display: 'flex',
+    alignContent: 'flex-start'
+  },
+  paragraphText: {
+    fontSize: 16
+  },
+  dashboardText: {
+    fontSize: 30,
+    marginLeft: 15,
+    marginTop: 10,
+    marginBottom: -10
   }
 });
