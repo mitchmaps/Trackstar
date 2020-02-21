0d0301cc5daef501cb264199228845de80c5c169
Gaby Pancu <gaby.pancu@shopify.com>
2020-02-21 12:42:54 -0500

    update course

--- Trackstar/models/Course.tsx
+++ Trackstar/models/Course.tsx
@@ -1,19 +1,21 @@
 import Evaluation from './Evaluation'
 
-class Course {
+export default class Course {
+    code: string;
     title: string;
     min_grade: number;
     grade: number;
     complete: boolean;
 
-    constructor(t: string, min_g: number, g: number, g: number, c:boolean) {
+    constructor(c: string, t: string, min_g: number) {
+        this.code = c;
         this.title = t;
         this.min_grade = min_g;
-        this.grade = g;
-        this.complete = c;
+        this.grade = 0;
+        this.complete = false;
     }
 
-    cur_grade(){
-        return Evaluation.grade;
-    }
+    // cur_grade(){
+    //     return Evaluation.grade;
+    // }
 }
