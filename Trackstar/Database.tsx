11a5e088da9c0bd5ab20cb9878a7b84f6de17680
Gaby Pancu <gaby.pancu@shopify.com>
2020-02-21 19:48:09 -0500

    sql update 2

--- Trackstar/Database.tsx
+++ Trackstar/Database.tsx
@@ -6,33 +6,29 @@ export default class Database {
   // TODO:
   // finish queries, then test them
   // figure out how to properly do callbacks for executeSql!!!!
+  // test that foreign key is enabled
 
   static init = () => {
     const db = SQLite.openDatabase("db.db");
     return new Promise((resolve) => {
       db.transaction(tx => {
-        // finish this, figure out date type
-        // tx.executeSql("create table if not exists Course (code text primary key, title text, min_grade float, grade float default 0, complete boolean default 0)")
-        tx.executeSql("create table if not exists Course (code text primary key, title text, min_grade float check (min_grade >= 0 & min_grade <= 100), grade float default 0 check (grade >= 0), complete boolean default 0)")
+        // add not null to min_grade
+        tx.executeSql("create table if not exists Course (code text primary key, title text not null, min_grade float check (min_grade >= 0 & min_grade <= 100), grade float default 0 check (grade >= 0), complete boolean default 0)")
         resolve()
       })
     }).then(() => {
-      console.log("created table")
-  //     return new Promise((resolve) => {
-  //       db.transaction(tx => {
-  //         // finish this, figure out date type
-  //         tx.executeSql("create table if not exists Evaluation (id integer primary key, title text not null, due_date datetime, weight number (check weight >= 0 && weight <= 100), grade float default 0 (check grade >= 0), complete boolean default 0, cou)")
-  //         resolve()
-  //       })
-  //     }).then(() => {
-  //       db.transaction(tx => {
-  //         // finish this, figure out date type
-  //         tx.executeSql("create table if not exists Task ()")
-  //       })
-  //     })
-  //   }
-  // )}
-    })}
+      return new Promise((resolve) => {
+        db.transaction(tx => {
+          tx.executeSql("create table if not exists Evaluation (id integer primary key, title text not null, due_date datetime not null, weight number not null, grade float default 0, complete boolean default 0, course_code text not null, foreign key(course_code) references Course(code), check (weight >= 0 & weight <= 100), check (grade >= 0))")
+          resolve()
+        })
+      }).then(() => {
+        db.transaction(tx => {
+          tx.executeSql("create table if not exists Task (id integer primary key, title text not null, due_date datetime, est_duration:number not null, priority number, complete boolean default 0, eval_id integer not null, foreign key(eval_id) references Evaluation(id))")
+        })
+      })
+    }
+  )}
 
   static deleteCourseTable = () => {
     const db = SQLite.openDatabase("db.db");
--- Trackstar/models/Task.tsx
+++ Trackstar/models/Task.tsx
@@ -2,7 +2,6 @@ import Constants from 'expo-constants';
 import * as SQLite from 'expo-sqlite';
 
 export default class Task {
-    id:number;
     title:string;
     due_date:Date;
     complete:boolean;
@@ -12,7 +11,6 @@ export default class Task {
     db = SQLite.openDatabase("db.db");
 
     constructor(t:string, d:Date, ed:number, p:number, ei:number) {
-        //this.id =
         this.title = t;
         this.due_date = d;
         this.est_duration = ed;
@@ -21,12 +19,10 @@ export default class Task {
         this.evaluation_id = ei;
     }
 
-  // TO DO:
-  // figure out how to do ID
   save() {
     this.db.transaction(
       tx => {
-        tx.executeSql("insert into Task (id, title, est_duration, priority, complete, evaluation_id) values (?, ?, ?, ?, ?, ?)", [this.id, this.title, this.est_duration, this.priority, this.complete, this.evaluation_id]);
+        tx.executeSql("insert into Task (title, est_duration, priority, complete, evaluation_id) values (?, ?, ?, ?, ?)", [this.title, this.est_duration, this.priority, this.complete, this.evaluation_id]);
         // tx.executeSql("select * from Course", [], (_, { rows }) =>
         //   console.log(JSON.stringify(rows))
         // );
