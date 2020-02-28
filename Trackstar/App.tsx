77dc85a0997ba73e1f1ea4e6f0812cba6e05ffc1
Gaby Pancu <gaby.pancu@shopify.com>
2020-02-28 15:14:03 -0500

    add icons

--- Trackstar/App.tsx
+++ Trackstar/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';// import {createStackNavigator} from 'react-navigation-stack';
+import { AntDesign } from '@expo/vector-icons';
 
 import HomeScreen from './screens/DashboardScreen';
 import CoursesSreen from './screens/CourseScreen';
@@ -11,7 +12,28 @@ const Tab = createBottomTabNavigator();
 
 function MyTabs() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'Courses') {
+            iconName = 'book';
+          } else {
+            iconName = 'calculator';
+          }
+
+          // You can return any component that you like here!
+          return <AntDesign name={iconName} size={size} color={color} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+      }}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Courses" component={CoursesSreen} />
       <Tab.Screen name="Grades" component={GradesScreen} />
