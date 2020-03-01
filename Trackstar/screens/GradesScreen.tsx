d4a66a2cad14bc00086a51b150cbec953afe8115
Nareen <Nareen.Khurshid@carleton.ca>
2020-03-01 00:19:50 -0500

    Math does not yet function

--- Trackstar/screens/GradesScreen.tsx
+++ Trackstar/screens/GradesScreen.tsx
@@ -1,14 +1,137 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View,TouchableOpacity } from 'react-native';
 import Styles from '../Styles/CourseStyles';
+import { TextInput } from 'react-native';
 
+//Code below Added by Nareen feb
 const GradesScreen = (props) => {
-  const navigation = props.navigation;
+const navigation = props.navigation;
+//  this.state = { text: '' };
+
     return (
       <View style={Styles.content}>
-        <Text>Grade calculation!</Text>
+                <Text>Grade calculation!</Text>
+	  <View style={{ flex: 1, flexDirection: 'row' }}>			
+	  <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+		<TextInput
+          style={{ height: 40 }}
+          placeholder="% of worth overall"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+	   </View>
+	   
+	   	<View style={{ flex: 1, flexDirection: 'row' }}>			
+	  <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+		<TextInput
+          style={{ height: 40 }}
+          placeholder="% of worth overall"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+	   </View>
+	   
+	   	  <View style={{ flex: 1, flexDirection: 'row' }}>			
+	  <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+		<TextInput
+          style={{ height: 40 }}
+          placeholder="% of worth overall"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+	   </View>
+	   
+	   	   	  <View style={{ flex: 1, flexDirection: 'row' }}>			
+	  <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+		<TextInput
+          style={{ height: 40 }}
+          placeholder="% of worth overall"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+	   </View>
+	   
+	   	   	  <View style={{ flex: 1, flexDirection: 'row' }}>			
+	  <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+		<TextInput
+          style={{ height: 40 }}
+          placeholder="% of worth overall"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+	   </View>
+	   
+	   	   	  <View style={{ flex: 1, flexDirection: 'row' }}>			
+	  <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+		<TextInput
+          style={{ height: 40 }}
+          placeholder="% of worth overall"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+	   </View>
+	   
+	   <View style={{ flex: 1, flexDirection: 'row' }}>			
+	  <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+		<TextInput
+          style={{ height: 40 }}
+          placeholder="% of worth overall"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+	   </View>
+	   
+
+      <Text>Total Earned is: </Text>
+				
+
+      <TouchableOpacity
+        style={{ backgroundColor: 'black' }}>
+        <Text style={{ fontSize: 20, color: '#fff' }}>calculate</Text>
+      </TouchableOpacity>
+	  
       </View>
+	  
+	  
     );
+	// TO DO: do the calculations using the fields (functionality)
+	// TO DO: save the data per course? and be able to get those numbers from those values and display them on the specific course's page
+	
 };
 
 export default GradesScreen;
