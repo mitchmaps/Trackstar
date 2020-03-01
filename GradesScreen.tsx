da9af9a413a08a4be8364fff1ec503a6a4a787b2
Nareen <Nareen.Khurshid@carleton.ca>
2020-02-29 23:08:48 -0500

    Input fields don't store values yet

--- GradesScreen.tsx
+++ GradesScreen.tsx
@@ -1,7 +1,3 @@
-//Button on screen
-//TO DO addding and formating input fields
-//TO DO adding "do calculation" code
-
 import React from 'react';
 import { Text, View,TouchableOpacity } from 'react-native';
 import Styles from '../Styles/CourseStyles';
@@ -10,10 +6,36 @@ import { TextInput } from 'react-native';
 //Code below Added by Nareen feb
 const GradesScreen = (props) => {
 const navigation = props.navigation;
-  
+//  this.state = { text: '' };
+
     return (
       <View style={Styles.content}>
                 <Text>Grade calculation!</Text>
+				
+	  <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+          //onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+         // onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter grade % earned"
+          //onChangeText={text => this.setState({ text })}
+          //value={this.state.text}
+        />
 
       <TouchableOpacity
         style={{ backgroundColor: 'black' }}>
@@ -24,7 +46,7 @@ const navigation = props.navigation;
 	  
 	  
     );
-	//Added the buttons and their functionality
+	// TO DO: do the calculations using the fields (functionality)
 	// TO DO: save the data per course? and be able to get those numbers from those values and display them on the specific course's page
 	
 };
