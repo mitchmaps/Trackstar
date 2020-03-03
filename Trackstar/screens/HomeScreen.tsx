0c3adf74a7f1e21d604271dd9846c1b2b8926d1c
Nareen <Nareen.Khurshid@carleton.ca>
2020-03-03 14:05:21 -0500

    (needs to be fixed) add mock tasks to home screen
    tasks method to make task new task ,save, add mock tasks to home screen 
    where does it go, does it go here?

--- Trackstar/screens/HomeScreen.tsx
+++ Trackstar/screens/HomeScreen.tsx
@@ -8,6 +8,19 @@ const HomeScreen = (props) => {
       <View style={Styles.content}>
         <Text>Home Screen!</Text>
       </View>
+      
+      //Where does this go
+      /*
+      //adding mock task
+      static mocktask() {
+      let task1 = new task (id, title, course, time);
+      let task2 = new task (id, title, course, time);
+      let task3 = new task (id, title, course, time);
+      let task4 = new task (id, title, course, time);
+      //console.log(course.code)
+      task.save()
+      }
+      */
     );
 };
 
