d992275dcd33df904790fbb493e6c31856f78506
Gaby Pancu <gaby.pancu@shopify.com>
2020-03-03 16:15:40 -0500

    styling

--- Trackstar/screens/TestScreen.tsx
+++ Trackstar/screens/TestScreen.tsx
@@ -5,7 +5,7 @@ import Course from '../models/Course';
 const TestScreen = (props) => {
   const navigation = props.navigation;
     return (
-      <TouchableOpacity onPress={() => {Course.all()}}>
+      <TouchableOpacity style={{marginTop: 100}}onPress={() => {Course.all()}}>
         <Text>All Courses</Text>
       </TouchableOpacity>
     );
