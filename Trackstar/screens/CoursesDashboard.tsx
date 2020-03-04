60803e618f221b20544d0b51f0de327bfbb33231
Mitchell Soares <mitchell.soares@shopify.com>
2020-03-04 12:42:23 -0500

    Fixed promise issue

--- Trackstar/screens/CoursesDashboard.tsx
+++ Trackstar/screens/CoursesDashboard.tsx
@@ -1,154 +1,145 @@
-import {useEffect, useState, useCallback} from 'react';
-import React from 'react';
+import { useEffect, useState, useCallback } from "react";
+import React from "react";
 
-import {SectionList, StyleSheet, ScrollView, Alert, Text, View , TouchableOpacity} from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
-import { Switch, Avatar, Button, Card, Title, Paragraph } from 'react-native-paper';
-import { red100 } from 'react-native-paper/lib/typescript/src/styles/colors';
-import {mockData_CourseDashboard} from "../mockData_CourseDashboard"
+import {
+  SectionList,
+  StyleSheet,
+  ScrollView,
+  Alert,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import {
+  Switch,
+  Avatar,
+  Button,
+  Card,
+  Title,
+  Paragraph
+} from "react-native-paper";
+import { red100 } from "react-native-paper/lib/typescript/src/styles/colors";
+import { mockData_CourseDashboard } from "../mockData_CourseDashboard";
 import Styles from "../Styles/CoursesDashboardStyles";
 // import { iOSUIKit } from 'react-native-typography';
 
-import Course from '../models/Course';
-
-
+import Course from "../models/Course";
 
 const CoursesDashboard = props => {
-  const [oldCourses, setOldCourses] = useState (true); // hook state for toggle
+  const [oldCourses, setOldCourses] = useState(true); // hook state for toggle
   const [formattedCourseData, setFormattedCourseData] = useState([]);
   const navigation = props.navigation;
 
-  // const getCourseData = useCallback(() => {
-  //   const formattedData = [];
-  //   ////////
-  //   const rawData = Course.all().then((data: Course[]) => {
-  //     data.forEach(course => {
-  //       const courseInfo = {
-  //         title: course.title,
-  //         data:
-  //         [{
-  //             code: course.code,
-  //             title: course.title,
-  //         }]
-  //       }
-  //       formattedData.push(courseInfo);
-  //     })
-  //     resolve(formattedData)
-  //     // setFormattedCourses(formattedData);
-  //   }
-  //   )
-  // }, []);
-
+  useEffect(() => {
+    const formattedCourses = formatData().then((data) => {
+      console.log("formatted");
+      console.log(data);
+      setFormattedCourseData(data);
+      console.log('state');
+      console.log(formattedCourseData);
+    })
+  }, []);
 
   const SingleItem = data => {
-
-  const item = data.item
-    if(item.color === "#999999" && oldCourses === false){ // only render on toggle if course is not pre-req
+    const item = data.item;
+    if (item.color === "#999999" && oldCourses === false) {
+      // only render on toggle if course is not pre-req
       return null;
-    }
-    else{
-      return(
-        <Card style = {[{backgroundColor: item.color}, Styles.container]}
-        onPress  ={() =>{
-        navigation.navigate("Course") }}>
-            <Card.Content style={{flex: 1, flexDirection: 'row'}}>
-            <View style={{backgroundColor: item.color}}>
-                <Title style={Styles.titleText}>{item.courseCode}</Title>
-                <Paragraph style={Styles.paragraphText}>{item.courseName}</Paragraph>
-                <Paragraph style={Styles.paragraphText}>{item.term}</Paragraph>
+    } else {
+      return (
+        <Card
+          style={[{ backgroundColor: item.color }, Styles.container]}
+          onPress={() => {
+            navigation.navigate("Course");
+          }}
+        >
+          <Card.Content style={{ flex: 1, flexDirection: "row" }}>
+            <View>
+              <Title style={Styles.titleText}>{item.code}</Title>
+              <Paragraph style={Styles.paragraphText}>
+                {item.title}
+              </Paragraph>
             </View>
-            </Card.Content>
+          </Card.Content>
         </Card>
       );
+    }
+  };
 
-    };
-  }
-
-  let realData = [];
-
-  const formattedCourses = formatData().then((data) => {
-    console.log('promise start');
-    console.log(data);
-    setFormattedCourseData(data);
-    console.log('promise end');
-  });
-
-  console.log('formatted');
-  console.log(realData);
-
-  return(
+  return (
     <LinearGradient
-    colors={['#bcf7ed', '#5273eb']}
-    style={{ flex: 1, flexDirection: 'column',
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "center",}}>
-
+      colors={["#bcf7ed", "#5273eb"]}
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
       <View style={Styles.dashboardRowOne}>
         <Text style={Styles.dashboardText}>Dashboard</Text>
         <Text style={Styles.dashboardText}>
-          {formattedCourseData[0].title}
+          {/* {formattedCourseData[0].title} */}
         </Text>
-        <Text style={Styles.switchText}>              Pre-Reqs </Text>
+        <Text style={Styles.switchText}> Pre-Reqs </Text>
         <Switch
           value={oldCourses}
-          onValueChange={() =>
-            { setOldCourses(!oldCourses) }
-          }/>
+          onValueChange={() => {
+            setOldCourses(!oldCourses);
+          }}
+        />
       </View>
 
-
       <SectionList
-        style={{marginBottom: 10}}
-        sections={mockData_CourseDashboard}
+        style={{ marginBottom: 10 }}
+        sections={formattedCourseData}
         renderItem={SingleItem}
       />
 
       <View style={Styles.buttonContentStyle}>
-        <TouchableOpacity onPress={() => {
-          props.navigation.navigate("Add")}}>
+        <TouchableOpacity
+          onPress={() => {
+            props.navigation.navigate("Add");
+          }}
+        >
           <View style={Styles.buttonContentStyle}>
-            <Button  icon="plus-circle" mode="contained" contentStyle={{marginLeft:10}} >
+            <Button
+              icon="plus-circle"
+              mode="contained"
+              contentStyle={{ marginLeft: 10 }}
+            >
               add course
             </Button>
           </View>
         </TouchableOpacity>
       </View>
-
     </LinearGradient>
-
-    );
-
+  );
 };
 
 async function formatData() {
   const formattedData = [];
 
   let rawData: Course[] = await Course.all();
-  console.log('raw');
+  console.log("raw");
   console.log(rawData);
 
-    rawData.forEach(course => {
-      const courseInfo = {
-        title: course.title,
-        data:
-        [{
-            code: course.code,
-            title: course.title,
-        }]
-      }
-      formattedData.push(courseInfo);
-    })
+  rawData.forEach(course => {
+    const courseInfo = {
+      title: course.title,
+      data: [
+        {
+          code: course.code,
+          title: course.title
+        }
+      ]
+    };
+    formattedData.push(courseInfo);
+  });
 
-    return formattedData;
-  }
+  return formattedData;
+}
 
-  function doStuffWithData(courses: Course[]) {
-
-  }
-
-  // console.log('in new function');
-  // console.log(formattedData[0]);
-  //return formattedData;
-
-  export default CoursesDashboard;
+export default CoursesDashboard;
