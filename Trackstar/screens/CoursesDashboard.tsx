4ad1796fa1927d856fa842d21881de293ced3a18
Gaby Pancu <gaby.pancu@shopify.com>
2020-03-04 10:02:44 -0500

    tried to use state to fix problem, did not succeed

--- Trackstar/screens/CoursesDashboard.tsx
+++ Trackstar/screens/CoursesDashboard.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useCallback} from 'react';
 import React from 'react';
 
 import {SectionList, StyleSheet, ScrollView, Alert, Text, View , TouchableOpacity} from 'react-native';
@@ -9,11 +9,36 @@ import {mockData_CourseDashboard} from "../mockData_CourseDashboard"
 import Styles from "../Styles/CoursesDashboardStyles";
 // import { iOSUIKit } from 'react-native-typography';
 
+import Course from '../models/Course';
+
 
 
 const CoursesDashboard = props => {
   const [oldCourses, setOldCourses] = useState (true); // hook state for toggle
+  const [formattedCourseData, setFormattedCourseData] = useState([]);
   const navigation = props.navigation;
+
+  // const getCourseData = useCallback(() => {
+  //   const formattedData = [];
+  //   ////////
+  //   const rawData = Course.all().then((data: Course[]) => {
+  //     data.forEach(course => {
+  //       const courseInfo = {
+  //         title: course.title,
+  //         data:
+  //         [{
+  //             code: course.code,
+  //             title: course.title,
+  //         }]
+  //       }
+  //       formattedData.push(courseInfo);
+  //     })
+  //     resolve(formattedData)
+  //     // setFormattedCourses(formattedData);
+  //   }
+  //   )
+  // }, []);
+
 
   const SingleItem = data => {
 
@@ -39,6 +64,18 @@ const CoursesDashboard = props => {
     };
   }
 
+  let realData = [];
+
+  const formattedCourses = formatData().then((data) => {
+    console.log('promise start');
+    console.log(data);
+    setFormattedCourseData(data);
+    console.log('promise end');
+  });
+
+  console.log('formatted');
+  console.log(realData);
+
   return(
     <LinearGradient
     colors={['#bcf7ed', '#5273eb']}
@@ -49,6 +86,9 @@ const CoursesDashboard = props => {
 
       <View style={Styles.dashboardRowOne}>
         <Text style={Styles.dashboardText}>Dashboard</Text>
+        <Text style={Styles.dashboardText}>
+          {formattedCourseData[0].title}
+        </Text>
         <Text style={Styles.switchText}>              Pre-Reqs </Text>
         <Switch
           value={oldCourses}
@@ -80,5 +120,35 @@ const CoursesDashboard = props => {
     );
 
 };
+
+async function formatData() {
+  const formattedData = [];
+
+  let rawData: Course[] = await Course.all();
+  console.log('raw');
+  console.log(rawData);
+
+    rawData.forEach(course => {
+      const courseInfo = {
+        title: course.title,
+        data:
+        [{
+            code: course.code,
+            title: course.title,
+        }]
+      }
+      formattedData.push(courseInfo);
+    })
+
+    return formattedData;
+  }
+
+  function doStuffWithData(courses: Course[]) {
+
+  }
+
+  // console.log('in new function');
+  // console.log(formattedData[0]);
+  //return formattedData;
 
   export default CoursesDashboard;
