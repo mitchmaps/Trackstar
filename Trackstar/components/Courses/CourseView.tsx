cacb8d035314aaeecb8623d4aa19c58d97cedb71
Mitchell Soares <mitchell.soares@shopify.com>
2020-03-04 21:13:33 -0500

    UI changes

--- Trackstar/components/Courses/CourseView.tsx
+++ Trackstar/components/Courses/CourseView.tsx
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { View, Text, ScrollView } from "react-native";
-import { Card, Divider, Title, Badge } from "react-native-paper";
+import { Card, Divider, Title, Badge, Paragraph } from "react-native-paper";
 import { iOSUIKit } from "react-native-typography";
 import Evaluation from "../../models/Evaluation";
 
@@ -11,48 +11,16 @@ export interface Props {
   minGrade: number;
 }
 
-// console.log(Evaluation.all());
-
 export default function CourseView({ code, name, term, minGrade }: Props) {
-  // const fakeEvaluations: Evaluation[] = [
-  //   {
-  //     title: "Midterm",
-  //     due_date: new Date("2020-03-13"),
-  //     complete: false,
-  //     weight: 35,
-  //     grade: 0,
-  //     course_code: "BIOL 1902"
-  //   },
-  //   {
-  //     title: "Final",
-  //     due_date: new Date("2020-04-10"),
-  //     complete: false,
-  //     weight: 65,
-  //     grade: 0,
-  //     course_code: "BIOL 1902"
-  //   },
-  //   {
-  //     title: "Test",
-  //     due_date: new Date("2020-03-06"),
-  //     complete: false,
-  //     weight: 0,
-  //     grade: 0,
-  //     course_code: "BIOL 1902"
-  //   }
-  // ];
-
   const [courseEvals, setCourseEvals] = useState([]);
 
   useEffect(() => {
-    console.log(code);
     const evalData = retrieveEvalData(code).then((data: Evaluation[]) => {
-      console.log('data from promise');
-      console.log(data);
       setCourseEvals(data);
     });
   }, []);
 
-  // replace with DB query when setup
+  const gradingSchemeMarkup = generateGradingSchemeMarkup(courseEvals);
   const evalMarkup = generateEvalMarkup(courseEvals);
 
   return (
@@ -66,14 +34,40 @@ export default function CourseView({ code, name, term, minGrade }: Props) {
       >
         <Text style={iOSUIKit.largeTitleEmphasized}>{code}</Text>
         <Text style={iOSUIKit.subhead}>{name}</Text>
-        <Text style={iOSUIKit.bodyEmphasized}>Grading scheme</Text>
+        <Text style={iOSUIKit.title3Emphasized}>Evaluations</Text>
+        <View style={{ paddingVertical: 20 }}>{gradingSchemeMarkup}</View>
+        <Text style={iOSUIKit.title3Emphasized}>Tasks</Text>
         {evalMarkup}
       </ScrollView>
     </View>
   );
 }
 
-// convert this to tasks
+function generateGradingSchemeMarkup(evals: Evaluation[]) {
+  const gradingSchemeMarkup = evals.reduce((allEvals, currEval) => {
+    const evalMarkup = (
+      <Card.Content>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between'}}>
+          <Paragraph>{currEval.title}</Paragraph>
+          <Badge visible={true} style={{backgroundColor: "#408ff7", fontWeight: 'bold', color: "#ffffff"}}>
+            {`${currEval.weight}%`}
+          </Badge>
+        </View>
+        <Divider />
+      </Card.Content>
+    );
+
+    allEvals.push(evalMarkup);
+    return allEvals;
+  }, []);
+
+  return (
+    <Card>
+      {gradingSchemeMarkup}
+    </Card>
+  );
+}
+
 function generateEvalMarkup(evals: Evaluation[]) {
   return evals.reduce((allEvals, currEval) => {
     const { title, due_date, weight } = currEval;
@@ -83,30 +77,31 @@ function generateEvalMarkup(evals: Evaluation[]) {
     const daysUntil = determineDaysUntilEval(formattedDate);
     const badgeText = `In ${daysUntil} days`;
 
-    const badgeColor = (daysUntil > 10) ? (
-      "#408ff7"
-    ) : (daysUntil <= 10 && daysUntil > 5) ? (
-      "#f48618"
-    ) : (
-      "#fc2525"
-    );
+    const badgeColor =
+      daysUntil > 10
+        ? "#408ff7"
+        : daysUntil <= 10 && daysUntil > 5
+        ? "#f48618"
+        : "#fc2525";
 
     const badgeMarkup = (
-      <Badge visible={true} style={{backgroundColor: badgeColor, color: '#ffffff'}}>
+      <Badge
+        visible={true}
+        style={{ backgroundColor: badgeColor, color: "#ffffff" }}
+      >
         {badgeText}
       </Badge>
     );
 
     const titleMarkup = (
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{ flex: 1, flexDirection: "row" }}>
         <Title>{title}</Title>
         {badgeMarkup}
       </View>
     );
 
-    // Chaneg how this looks
     const evalMarkup = (
-      <View key={title} style={{paddingVertical: 5}}>
+      <View key={title} style={{ paddingVertical: 5 }}>
         <Card>
           <Card.Content>
             <Card.Title title={title} subtitle={`Worth ${weight}%`} />
@@ -134,7 +129,7 @@ function determineDaysUntilEval(evalDate: Date) {
         currDate.getMonth(),
         currDate.getDate()
       )) /
-      oneDayInMs
+    oneDayInMs
   );
 }
 
