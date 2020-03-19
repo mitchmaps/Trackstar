ab4b006029d92c3e3b9a3c7b6d9efd7ee2cd9b96
Nareen <Nareen.Khurshid@carleton.ca>
2020-03-19 15:46:25 -0400

    Update GradesScreen.tsx

--- Trackstar/screens/GradesScreen.tsx
+++ Trackstar/screens/GradesScreen.tsx
@@ -4,9 +4,10 @@ import { Text, View } from 'react-native';
 import GradesForm from '../components/GradesForm';
 
 // TO DO:
+// error detection (right now we handle it but don't bug the user about it)
 //  - non-numerical/empty values
+//  - not adding up to 100, should be working
 //  - skipping rows
-
 // auto-population
 
 const GradesScreen = (props) => {
