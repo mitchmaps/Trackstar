2056700a0e49b1f39808c5c06214983b6a18aaf7
Gaby Pancu <gaby.pancu@shopify.com>
2020-03-19 11:56:28 -0400

    implement user mapper and make createTable private

---
+++ Trackstar/data_mappers/UserMapperImpl.tsx
@@ -0,0 +1,48 @@
+import UserMapper from "./UserMapper";
+import User from "../models/User";
+import DBConnection from "../DBConnection";
+
+
+export default class UserMapperImpl implements UserMapper {
+  db = DBConnection.open()
+
+  constructor() {
+    this.createTable()
+    this.insert()
+
+  }
+
+  update(user: User): void {
+    this.db.transaction(
+      tx => {
+        tx.executeSql("update User set est_accuracy=?", [user.estimationAccuracy]);
+      },
+      null
+    );
+  }
+
+  getUser(): Promise<User> {
+    return new Promise((resolve) => {
+      this.db.transaction(tx => {
+        tx.executeSql("select * from User", [], (_, { rows: { _array } }) => {
+          resolve(_array[0])
+        })
+      })
+    })
+  }
+
+  private insert(): void {
+    this.db.transaction(
+      tx => {
+        tx.executeSql("insert into User (est_accuracy) values (100)");
+      },
+      null
+    );
+  }
+
+  private createTable(): void {
+    this.db.transaction(tx => {
+      tx.executeSql("create table if not exists User (est_accuracy)")
+    })
+  }
+}
