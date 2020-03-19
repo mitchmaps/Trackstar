616a0ad3e3eb9f22408d8ffdbd7b0a25700d3b72
haohaodu <hdu2899@gmail.com>
2020-03-19 17:41:14 -0400

    fixed bugs

--- Trackstar/models/TaskPrioritizer.tsx
+++ Trackstar/models/TaskPrioritizer.tsx
@@ -1,4 +1,4 @@
-import Task from './Task';
+import Task from './Task'
 
 export default class TaskPrioritizer{
 
@@ -11,6 +11,8 @@ export default class TaskPrioritizer{
         let priorityCounter = 0;
 
         for (let index = 0; index < t.length; index++) {
+
+            priorityCounter = 0;
             
             // find metrics to be later put into buckets
             DueDate = this.date_diff_indays(new Date(),t[index].due_date); 
@@ -19,21 +21,19 @@ export default class TaskPrioritizer{
             // calculate priority
             priorityCounter += this.due_date_levels(DueDate)
             priorityCounter += this.duration_levels(t[index].est_duration)
-            // priorityCounter += this.weighting_levels(evaluation.weight);
 
-            // find average and redefine the priority as that number
-            priorityCounter/=3;
-            t[index].priority = parseInt(priorityCounter.toFixed(2));
+            // final calculation for priority
+            priorityCounter/=2.0000;
 
             // pass in the priority values into a list 
-            sortList = this.insertList(sortList, parseInt(priorityCounter.toFixed(2)), t[index])
+            sortList = this.insertList(sortList, priorityCounter);
             
             // pass in the priority value as key, and object as value
-            mappingList = this.insertKey(mappingList, parseInt(priorityCounter.toFixed(2)), t[index])
+            mappingList = this.insertKey(mappingList, parseFloat(priorityCounter.toFixed(5)), t[index])
         }
 
         // sort the priority values list
-        sortList.sort(function(a,b){return b-a});
+        sortList = sortList.sort(function(a,b){return b-a});
 
         // populate a new sorted list of tasks based off of priority
         let returnValue = [];
@@ -47,9 +47,9 @@ export default class TaskPrioritizer{
 
 
     private date_diff_indays = (date1: Date, date2: Date) => {
-        let dt1 = new Date(date1);
-        let dt2 = new Date(date2);
-        return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+        let dt1: Date = new Date(date1);
+        let dt2: Date = new Date(date2);
+        return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) /(1000 * 60 * 60 * 24));
     }
 
     private insertKey = (tempMap: Map<number, Task>, key: number, value: Task) => {
@@ -60,13 +60,12 @@ export default class TaskPrioritizer{
             }
             else
             {
-                value.priority = key;
                 tempMap.set(key, value)
                 return tempMap;
             }
         }
     }
-    private insertList = (tempList, value, task) => {
+    private insertList = (tempList, value) => {
         while(true){
             if(tempList.includes(value))
             {   
@@ -74,7 +73,6 @@ export default class TaskPrioritizer{
             }
             else
             {
-                task.priority = value;
                 tempList.push(value)
                 return tempList;
             }
@@ -82,22 +80,8 @@ export default class TaskPrioritizer{
     }
     
     private due_date_levels = value => {
-        if(value<=3)return 5
-        else if (value<=7)return 4
-        else if (value<=14)return 3
-        else if (value<=28)return 2
-        else return 1
+        return 10000 - value;
     }
-    
-    /*
-    weighting_levels = value =>{
-        if(value<=5)return 1
-        else if (value<=10)return 2
-        else if (value<=20)return 3
-        else if (value<=40)return 4
-        else return 5
-    }
-    */
 
     private duration_levels = value => {
         if(value<=30)return 1
