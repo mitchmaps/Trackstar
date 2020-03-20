6dee4e6f83c047f1b237e70d519d642ee3f06fae
Nareen <Nareen.Khurshid@carleton.ca>
2020-03-20 08:58:49 -0400

    return GradeInfo instance

--- Trackstar/components/GradesForm.tsx
+++ Trackstar/components/GradesForm.tsx
@@ -3,22 +3,16 @@ import { Text, View,TouchableOpacity, ImageEditor, Alert } from 'react-native';
 import { TextInput } from 'react-native';
 
 export default class GradesForm extends React.Component {
-  state: {
-    grades_and_weights: number[][],
-    desired_grade: number,
-    avg_grade: number,
-    combined_weight: number,
-    needed_grade: number,
-    remaining_weight: number
-  }
+
+//GradeInfo instance 
+
+
 
   constructor(props) {
     super(props);
     // this.update = this.update.bind(this);
     this.field = this.field.bind(this);
     this.handle_submit = this.handle_submit.bind(this)
-	  //TO DO: Calculate was moved out of here and into GradeCalulate.tsx 
-	  //so you need to do this line in there too or update it from there
     //this.calculate = this.calculate.bind(this)
 
     this.state = {
@@ -61,7 +55,7 @@ export default class GradesForm extends React.Component {
   }
 
   handle_submit() {
-    this.calculate()
+    this.gradeCalculator.calculate()
     if (this.state.combined_weight > 100) {
       Alert.alert(
         "The combined weights of these evaluations surpasses 100%.",
@@ -87,35 +81,7 @@ export default class GradesForm extends React.Component {
     }
   }
 
-  /* The calculations are done in GradesCalculator.tsx now
-  calculate() {
-    let avg_grade = 0;
-    let combined_grade = 0;
-    let combined_weight = 0;
-    let needed_grade = 0;
-    let remaining_weight = 0;
-
-    for (let i = 0; i < this.state.grades_and_weights.length; i++) {
-      let curEval = this.state.grades_and_weights[i];
-      if (curEval == undefined || Number.isNaN(curEval[0]) || Number.isNaN(curEval[1]) || curEval[0] == undefined || curEval[1] == undefined) {
-        continue;
-      }
-
-      combined_grade += curEval[0]*(curEval[1]/100);
-      combined_weight += curEval[1];
-    }
-
-    avg_grade = combined_grade/(combined_weight/100);
-    remaining_weight = 100 - combined_weight;
-    needed_grade = (this.state.desired_grade - combined_grade)/(remaining_weight/100);
-
-    this.state.avg_grade = parseFloat(avg_grade.toFixed(2));
-    this.state.combined_weight = parseFloat(combined_weight.toFixed(2));
-    this.state.needed_grade = parseFloat(needed_grade.toFixed(2));
-    this.state.remaining_weight = parseFloat(remaining_weight.toFixed(2));
-  }
-  */
-
+  /* The calculations are done in GradesCalculator.tsx now*/
 
   render() {
     return (
