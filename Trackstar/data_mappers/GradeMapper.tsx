78cc58a3e9e6ba592e9ede34070a9b9bbcd20ace
Nareen <Nareen.Khurshid@carleton.ca>
2020-03-23 22:41:09 -0400

    Update GradeMapper.tsx

--- Trackstar/data_mappers/GradeMapper.tsx
+++ Trackstar/data_mappers/GradeMapper.tsx
@@ -1,11 +1,13 @@
 import GradeInfo from "../components/GradeInfo";
 
 //setting up a mapper for the GradeInfo
+//what is it mapping? Should I make a Grade model?
+//Maybe it should be a Grade model instead of going into GradeInfo
 
 export default interface GradeMapper {
-  //insert(c: GradeInfo): void;
-  //update(c: GradeInfo): void;
-  //delete(c: GradeInfo): void;
-  //all(): Promise<GradeInfo[]>;
-  //find(c: string): Promise<GradeInfo>;
+  insert(c: GradeInfo): void;
+  update(c: GradeInfo): void;
+  delete(c: GradeInfo): void;
+  all(): Promise<GradeInfo[]>;
+  find(c: string): Promise<GradeInfo>;
 }
