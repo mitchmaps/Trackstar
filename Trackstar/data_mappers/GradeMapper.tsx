ccceda2852df2a4cb5f48a3e49719bc81f268948
Nareen <Nareen.Khurshid@carleton.ca>
2020-03-23 22:42:45 -0400

    Update GradeMapper.tsx

--- Trackstar/data_mappers/GradeMapper.tsx
+++ Trackstar/data_mappers/GradeMapper.tsx
@@ -1,8 +1,8 @@
-import GradeInfo from "../components/GradeInfo";
+import {GradeInfo} from "../components/GradeInfo";
 
 //setting up a mapper for the GradeInfo
-//what is it mapping? Should I make a Grade model?
-//Maybe it should be a Grade model instead of going into GradeInfo
+//what is it mapping? Should I make a Grade in the model folder
+//Maybe it should be in model to match formatting instead of going into GradeInfo in components
 
 export default interface GradeMapper {
   insert(c: GradeInfo): void;
