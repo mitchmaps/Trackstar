b2a28f71ba7f9af87c160ca36afb0569e1d561e6
Nareen <Nareen.Khurshid@carleton.ca>
2020-03-23 22:26:31 -0400

    Create GradeMapper.tsx
    create Grade mapper for the Grade Info
    The idea is to make use of Gaby's mapper to autofill grades into the calculator

---
+++ Trackstar/data_mappers/GradeMapper.tsx
@@ -0,0 +1,11 @@
+import GradeInfo from "../components/GradeInfo";
+
+//setting up a mapper for the GradeInfo
+
+export default interface GradeMapper {
+  //insert(c: GradeInfo): void;
+  //update(c: GradeInfo): void;
+  //delete(c: GradeInfo): void;
+  //all(): Promise<GradeInfo[]>;
+  //find(c: string): Promise<GradeInfo>;
+}
