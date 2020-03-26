a9db692310593b848cc8fab278fbe2cb4a89e4f4
haohaodu <hdu2899@gmail.com>
2020-03-25 22:11:45 -0400

    cleaned up task prioritizer code

--- Trackstar/models/TaskPrioritizer.tsx
+++ Trackstar/models/TaskPrioritizer.tsx
@@ -4,8 +4,9 @@ export default class TaskPrioritizer{
 
     prioritize = (t: Task[]) => {
 
-        let sortList = [];
-        let mappingList = new Map();
+        let returnValue = [];
+        let sortList = []; // take all priority values and put them into a list, to be sorted
+        let mappingList = new Map(); // attach all priority values with their respective tasks into a map
 
         let DueDate;
         let priorityCounter = 0;
@@ -16,27 +17,22 @@ export default class TaskPrioritizer{
             
             // find metrics to be later put into buckets
             DueDate = this.date_diff_indays(new Date(),t[index].due_date); 
-            // let evaluation: Evaluation  = evalMapper.find(t[index].evaluation_id); 
 
             // calculate priority
             priorityCounter += this.due_date_levels(DueDate)
             priorityCounter += this.duration_levels(t[index].est_duration)
-
-            // final calculation for priority
             priorityCounter/=2.0000;
 
-            // pass in the priority values into a list 
+            // pass in the priority values into a list , and then priority + the task objects into a map
             sortList = this.insertList(sortList, priorityCounter);
-            
-            // pass in the priority value as key, and object as value
             mappingList = this.insertKey(mappingList, parseFloat(priorityCounter.toFixed(5)), t[index])
         }
 
         // sort the priority values list
         sortList = sortList.sort(function(a,b){return b-a});
 
-        // populate a new sorted list of tasks based off of priority
-        let returnValue = [];
+        // populate a new sorted list of tasks based off of our sorted list
+        // use our sorted list values as keys to retrieve the actual task objects
         sortList.forEach(element => {
             returnValue.push(mappingList.get(element))
         });
@@ -46,18 +42,18 @@ export default class TaskPrioritizer{
     }
 
 
+    // difference between two days
     private date_diff_indays = (date1: Date, date2: Date) => {
         let dt1: Date = new Date(date1);
         let dt2: Date = new Date(date2);
         return Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate())) /(1000 * 60 * 60 * 24));
     }
 
+    // insert a task as the value, and its priority as its key
     private insertKey = (tempMap: Map<number, Task>, key: number, value: Task) => {
         while(true){
             if(tempMap.has(key))
-            {
                 key+=0.01
-            }
             else
             {
                 tempMap.set(key, value)
@@ -65,12 +61,13 @@ export default class TaskPrioritizer{
             }
         }
     }
+
+    // populate an entire list of priority values, if a value already exists add 0.01 to its value before pushing it
     private insertList = (tempList, value) => {
         while(true){
             if(tempList.includes(value))
-            {   
                 value+=0.01
-            }
+            
             else
             {
                 tempList.push(value)
@@ -79,10 +76,12 @@ export default class TaskPrioritizer{
         }
     }
     
+    // unfair to divide due dates into buckets so decided to create generic counter instead
     private due_date_levels = value => {
         return 10000 - value;
     }
 
+    // estimated duration bucketing
     private duration_levels = value => {
         if(value<=30)return 1
         else if (value<=60)return 2
