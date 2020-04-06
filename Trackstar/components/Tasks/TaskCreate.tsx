d184ae97994e6bea9672b8528faa06f49c72f95c
Gaby Pancu <gaby.pancu@shopify.com>
2020-04-05 22:37:49 -0400

    uncomment

--- Trackstar/components/Tasks/TaskCreate.tsx
+++ Trackstar/components/Tasks/TaskCreate.tsx
@@ -122,9 +122,8 @@ export default class TaskCreate extends React.Component {
           value={duration}
         />
         <Text>
-          {/* {User.getInstance().estimationAccuracy >= 100 ? `Heads up! You typically overestimate by about ${(Math.floor(User.getInstance().estimationAccuracy-100))}%`
-            : `Heads up! You typically underestimate by about ${Math.floor(Math.abs(User.getInstance().estimationAccuracy))}%` } */}
-        Heads up! You typically underestimate by about 20%
+          {User.getInstance().estimationAccuracy >= 100 ? `Heads up! You typically overestimate by about ${(Math.floor(User.getInstance().estimationAccuracy-100))}%`
+            : `Heads up! You typically underestimate by about ${Math.floor(Math.abs(User.getInstance().estimationAccuracy))}%` }
        </Text>
         </Card.Content>
       </Card>
